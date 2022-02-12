a64bf233b354e62dc9fda0506bbe2de138df6f68
Ri-yan <mohdriyan08011995@gmail.com>
2022-02-12 14:10:09 +0530

    Updates

---
+++ precache-manifest.ead7c9d54db424bfb952f486bce2a01b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9a7a20e155f51563ae4d0cd089479e3",
+    "url": "/Lets-Chatt/index.html"
+  },
+  {
+    "revision": "e91628718e70fa70eb3b",
+    "url": "/Lets-Chatt/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/Lets-Chatt/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/Lets-Chatt/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "e91628718e70fa70eb3b",
+    "url": "/Lets-Chatt/static/js/main.c5818a9e.chunk.js"
+  },
+  {
+    "revision": "ec07ba26c9ff50a9e7d5",
+    "url": "/Lets-Chatt/static/js/runtime~main.9d833b6e.js"
+  }
+]);
